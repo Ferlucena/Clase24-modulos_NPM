68927ffaf8f3b2fee79bd628809ab83afaf36a97
Ferchu Lucena <ferlucena@hotmail.com>
2024-02-05 16:19:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /**
- * Familiarizarse con npm y el uso de paquetes npm en proyectos de Node.js.
+ * Objetivo: Familiarizarse con npm y el uso de paquetes npm en proyectos de Node.js.
  * 1- Inicializar un proyecto Node.js mediante npm init-y
  * 2- Vamos a instalar un paquete npm muy simple llamado lodash, npm install lodash
  * 3- Crea un archivo app.js importamos lodash y utilizamos la funcion random para obtener un número matemático
